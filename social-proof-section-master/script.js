1b00937cb2a10761455cf48514a3c0adc44ae4b2
Arushi Malhotra <arushimalhotra2610@gmail.com>
2024-04-15 09:53:53 +0530

    added js to the testimonials

--- social-proof-section-master/script.js
+++ social-proof-section-master/script.js
@@ -1,22 +1,41 @@
 const testimonials = [
     {
-        image: "images/image-colton.jpg",
+        image: `http://127.0.0.1:5500/social-proof-section-master/images/image-colton.jpg`,
         name: "Colton Smith",
         verified: "Verified Buyer",
         feedback: "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!"
     },
 
     {
-        image: "images/image-irene.jpg",
+        image: `http://127.0.0.1:5500/social-proof-section-master/images/image-irene.jpg`,
         name: "Irene Roberts",
         verified: "Verified Buyer",
         feedback: "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery."
     },
 
     {
-        image: "images/image-anne.jpg",
+        image: `http://127.0.0.1:5500/social-proof-section-master/images/image-anne.jpg`,
         name: "Anne Wallace",
         verified: "Verified Buyer",
         feedback: "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"
     }
 ]
+
+const containerTemplate = (testimonial) =>
+`<div class="container">
+<div class="image"><img src=${testimonial.image}></div>
+<div class="name">${testimonial.name}</div>
+<div class="verified">${testimonial.verified}</div>
+<div class="feedback">${testimonial.feedback}</div>
+</div>`;
+
+let output = "";
+
+for (const testimonial of testimonials){
+    const filledTemplate = containerTemplate(testimonial);
+    output += filledTemplate;
+}
+
+console.log(output);
+
+document.getElementById("testimonials-container").innerHTML = output;
